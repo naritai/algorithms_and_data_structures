60076d3e2ba43db29447b22ec4c5697d23d7b02b
leekar0 <leekar0@yandex.ru>
2022-10-23 00:29:18 +0600

    singly linked list + push method

---
+++ singly-linked-list/index.js
@@ -0,0 +1,37 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.length = 0;
+    this.head = null;
+    this.tail - null;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+
+// check
+
+const list = new SinglyLinkedList();
+list.push('hello');
+list.push('there');
+list.push('world!');
+
+console.log(list);
